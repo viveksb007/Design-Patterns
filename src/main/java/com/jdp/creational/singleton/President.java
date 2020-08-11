2c0f2dae3dd018c7016f99a88d4a31138b4417fe
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2020-08-11 18:20:34 +0530

    prevent reflection breaking and made thread-safe

--- src/main/java/com/jdp/creational/singleton/President.java
+++ src/main/java/com/jdp/creational/singleton/President.java
@@ -2,17 +2,22 @@ package com.jdp.creational.singleton;
 
 class President {
 
-    private static President instance;
+    private static volatile President instance;
 
     private final String name;
 
     private President(String name) {
+        if (instance != null) {
+            throw new RuntimeException("Use getInstance() to get single instance of this class");
+        }
         this.name = name;
     }
 
     public static President getInstance() {
         if (instance == null) {
-            instance = new President("viveksb007");
+            synchronized (President.class) {
+                if (instance != null) instance = new President("viveksb007");
+            }
         }
         return instance;
     }
